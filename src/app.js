edae0aab06d8fe0770c7014af6ced23f78a1fee2
Anthony Wiloughby <awilloughby@bestunion.com>
2018-01-25 17:57:44 -0500

    creating the random number for the key and making the what should i do button. making it disabled dynamically

--- src/app.js
+++ src/app.js
@@ -25,6 +25,12 @@ const removeAll = () => {
     render();
 }
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
@@ -33,7 +39,7 @@ const render = () => {
             <h1>{app.title}</h1> 
             {app.subTitle && <p>{app.subTitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options!' : 'No options'}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={removeAll}>Remove All</button>
             <ol>
                 {
