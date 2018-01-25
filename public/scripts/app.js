b4c87258d48889d269052a4a6842fe77bca890ec
Anthony Wiloughby <awilloughby@bestunion.com>
2018-01-25 17:41:49 -0500

    created the dynamic list from an array with jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,6 +29,8 @@ var removeAll = function removeAll() {
 
 var appRoot = document.getElementById('app');
 
+var numbers = [55, 101, 1000];
+
 var render = function render() {
     var template = React.createElement(
         'div',
@@ -61,16 +63,13 @@ var render = function render() {
         React.createElement(
             'ol',
             null,
-            React.createElement(
-                'li',
-                null,
-                'Item one'
-            ),
-            React.createElement(
-                'li',
-                null,
-                'Item two'
-            )
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    option
+                );
+            })
         ),
         React.createElement(
             'form',
--- src/app.js
+++ src/app.js
@@ -27,6 +27,8 @@ const removeAll = () => {
 
 const appRoot = document.getElementById('app');
 
+const numbers = [55, 101, 1000]
+
 const render = () => {
     const template = (
         <div>
@@ -35,9 +37,17 @@ const render = () => {
             <p>{app.options.length > 0 ? 'Here are your options!' : 'No options'}</p>
             <p>{app.options.length}</p>
             <button onClick={removeAll}>Remove All</button>
+            {
+                // numbers.map((number) => {
+                //     return <p key={number}>Number: {number}</p>
+                // })
+            }
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+                {
+                    app.options.map((option) => {
+                        return <li key={option}>{option}</li>
+                    })
+                }
             </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option" />
