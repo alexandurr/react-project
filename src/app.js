ee8f5e2c67072e428352e1fa39e3588ae33eb4d6
Anthony Wiloughby <awilloughby@bestunion.com>
2018-01-30 17:36:26 -0500

    adding more react componets and nesting compnents

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -14,6 +27,17 @@ class Action extends React.Component {
         return (
             <div>
                 <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Options component here</p>
+                <Option />
             </div>
         );
     }
@@ -39,13 +63,4 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Option />
-        <AddOption />
-    </div>
-)
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
