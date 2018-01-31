501c929c1ee3bf24a1f417e42b1a79d288d4e081
Anthony Wiloughby <awilloughby@bestunion.com>
2018-01-31 15:51:17 -0500

    changing me setState function to arrow function

--- src/app.js
+++ src/app.js
@@ -9,11 +9,7 @@ class IndecisionApp extends React.Component {
         }
     }
     handleDeleteOptions() {
-        this.setState(() => {
-            return {
-                options: []
-            };
-        });
+        this.setState(() => ({ options: [] }));
     }
     handlePick() {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
@@ -27,11 +23,7 @@ class IndecisionApp extends React.Component {
             return 'This option already exisit';
         }
 
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat([option])
-            }
-        })
+        this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
     }
     render() {
         const subTitle = "Put your life in the hands of a computer!"
@@ -115,10 +107,8 @@ class AddOption extends React.Component {
 
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
-        
-        this.setState(() => {
-            return { error };
-        });
+
+        this.setState(() => ({ error }));
     } 
     render() {
         return (
