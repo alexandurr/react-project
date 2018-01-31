e442a668636fb203d9f5cac07f4b3ca3207fc4c9
Anthony Wiloughby <awilloughby@bestunion.com>
2018-01-31 11:28:43 -0500

    updating the visibility toggle example to use a react component with a state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -33,7 +33,7 @@ const render = () => {
 render();
 
 */
-
+/*
 let visiablilty = false;
 
 const toggleVisibility = () => {
@@ -60,3 +60,38 @@ const render = () => {
 }
 
 render();
+*/
+
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>Show Details</button>
+                {this.state.visibility && (
+                    <div>
+                        <p>Hey, there are some details!</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
